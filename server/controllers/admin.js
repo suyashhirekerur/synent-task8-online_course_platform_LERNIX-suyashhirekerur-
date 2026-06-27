212ecc986ac81ec41b95f35177629b9a3506b017
Suyash Hirekerur <shirekerur22@outlook.com>
2026-06-27 12:05:20 +0530

    Added Lectures configurations

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,5 +1,6 @@
 import TryCatch from "../middlewares/Trycatch.js"
 import { Courses } from "../models/Courses.js"
+import { Lecture } from "../models/Lecture.js";
 
 export const createCourse = TryCatch(async (req, res) => {
     const { title, description, category, createdBy, duration, price } = req.body
@@ -18,5 +19,29 @@ export const createCourse = TryCatch(async (req, res) => {
 
     res.status(201).json({
         message: "Course Created Successfully",
+    });
+});
+
+export const addlecture = TryCatch(async (req, res) => {
+    const course = await Courses.findById(req.params.id)
+
+    if (!course) return res.status(404).json({
+        message: "No course with this id",
+    });
+
+    const { title, description } = req.body
+
+    const file = req.file
+
+    const lecture = await Lecture.create({
+        title,
+        description,
+        video: file?.path,
+        course: course._id,
+    });
+
+    res.status(201).json({
+        message: "Lecture Added Successfully",
+        lecture,
     });
 });
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,9 +3,11 @@ import { isAuth } from "../middlewares/isAuth.js";
 import { createCourse } from "../controllers/admin.js";
 import { isAdmin } from "../middlewares/isAuth.js";
 import { uploadFiles } from "../middlewares/multer.js";
+import { addlecture } from "../controllers/admin.js";
 
 const router = express.Router()
 
 router.post('/courses/new', isAuth, isAdmin, uploadFiles, createCourse)
+router.post('/courses/:id', isAuth, isAdmin, uploadFiles, addlecture)
 
 export default router;
