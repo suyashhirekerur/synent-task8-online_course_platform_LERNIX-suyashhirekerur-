b6ab01cc3eb72fd04319b0c0cdb2751c5dec1362
Suyash Hirekerur <shirekerur22@outlook.com>
2026-07-05 11:00:08 +0530

    Remove localhost from CORS allowed origins

--- server/index.js
+++ server/index.js
@@ -21,7 +21,6 @@ app.use(express.json());
 app.use(cors({
     origin: [
         "https://lernix-frontend.onrender.com",
-        "http://localhost:5173",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
