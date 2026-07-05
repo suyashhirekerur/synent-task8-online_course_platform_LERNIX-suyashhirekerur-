06cbffb18634597ab7548939e0b91b1c99944319
Suyash Hirekerur <shirekerur22@outlook.com>
2026-07-05 11:24:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin: [
-        "http://localhost:5173",
+        "https://lernix-an-online-course-platform.onrender.com",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
